c9120ce647d58a2dd74999b5cfa388bf73678a96
Saed Abukar <saed.a.abukar@gmail.com>
2024-10-24 12:33:04 +0300

    2.2 done

---
+++ Module2/2.2/src/CategorizingItems.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CategorizingItems {
+    public static void main(String[] args) {
+        GroceryListManager groceryListManager = new GroceryListManager();
+
+        groceryListManager.addItem("Apple", 1, "Fruits");
+        groceryListManager.addItem("Milk", 2, "Dairy");
+        groceryListManager.addItem("Bread", 3, "Bakery");
+
+        System.out.println("Grocery List:");
+
+        int i = 1;
+        for (Map.Entry<String, HashMap<String, Double>> entry : groceryListManager.displayList().entrySet()) {
+            String category = entry.getKey();
+            String itemName = "";
+            double price = 0;
+            for (Map.Entry<String, Double> item : entry.getValue().entrySet()) {
+                itemName = item.getKey();
+                price = item.getValue();
+            }
+            System.out.printf("%d. %s, %s, %.2f%n", i, category, itemName, price);
+            i++;
+        }
+        System.out.println(" ");
+        System.out.printf("Is \"Apple\" in the grocery list? %s%n", groceryListManager.checkItem("apple"));
+        System.out.println(" ");
+        System.out.println("Removing \"Apple\" from the list...");
+        groceryListManager.removeItem("Apple");
+        System.out.println(" ");
+        System.out.println("Updated Grocery List:");
+        i = 1;
+        for (Map.Entry<String, HashMap<String, Double>> entry : groceryListManager.displayList().entrySet()) {
+            String category = entry.getKey();
+            String itemName = "";
+            double price = 0;
+            for (Map.Entry<String, Double> item : entry.getValue().entrySet()) {
+                itemName = item.getKey();
+                price = item.getValue();
+            }
+            System.out.printf("%d. %s, %s, %.2f%n", i, category, itemName, price);
+            i++;
+        }
+        double cost = groceryListManager.calculateCost();
+        System.out.printf("Total cost: %.2f%n", cost);
+
+        System.out.printf("The items in this category: %s%n", groceryListManager.displayCategory("Fruits"));
+    }
+}
--- Module2/2.2/src/GroceryListManager.java
+++ Module2/2.2/src/GroceryListManager.java
@@ -1,37 +1,72 @@
 import java.util.HashMap;
+import java.util.Objects;
 
 public class GroceryListManager {
-    private HashMap<String, Double> groceryList = new HashMap<>();
+    private HashMap<String, HashMap<String, Double>> groceryList = new HashMap<>();
     private double totalPrice;
+    private String fruits = "Fruits";
+    private String dairy = "Dairy";
+    private String bakery = "Bakery";
 
-    public void addItem(String item, double price) {
-        if (groceryList.containsKey(item)) {
+    public void addItem(String name, double price, String category) {
+        if (!Objects.equals(category, fruits) && !Objects.equals(category, dairy) && !Objects.equals(category, bakery)) {
             return;
+        }
+        if (groceryList.containsKey(category)) {
+            groceryList.get(category).put(name, price);
         } else {
-            groceryList.put(item, price);
+            HashMap<String, Double> item = new HashMap<>();
+            item.put(name, price);
+            groceryList.put(category, item);
         }
     }
 
     public void removeItem(String item) {
-        if (groceryList.containsKey(item)) {
-            groceryList.remove(item);
-        } else {
-            return;
+        String itemMatch = "";
+        String categoryMatch = "";
+        boolean found = false;
+        for (String category : groceryList.keySet()) {
+            for (String name : groceryList.get(category).keySet()) {
+                if (name.equals(item)) {
+                    itemMatch = name;
+                    categoryMatch = category;
+                    found = true;
+                }
+            }
         }
+        if (found) {
+            groceryList.get(categoryMatch).remove(itemMatch);
+        }
+
     }
 
-    public HashMap<String, Double> displayList() {
+    public HashMap<String, HashMap<String, Double>> displayList() {
         return groceryList;
     }
 
     public boolean checkItem(String item) {
-        return groceryList.containsKey(item);
+        boolean found = false;
+        for (String category : groceryList.keySet()) {
+            for (String name : groceryList.get(category).keySet()) {
+                if (name.equals(item)) {
+                    found = true;
+                }
+            }
+        }
+        return found;
     }
 
     public double calculateCost() {
-        for (Double price : groceryList.values()) {
-            totalPrice += price;
+        totalPrice = 0;
+        for (String category : groceryList.keySet()) {
+            for (String name : groceryList.get(category).keySet()) {
+                totalPrice += groceryList.get(category).get(name);
+            }
         }
         return totalPrice;
+    }
+
+    public HashMap<String, Double> displayCategory(String category) {
+        return groceryList.get(category);
     }
 }
